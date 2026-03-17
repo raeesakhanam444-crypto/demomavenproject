fa682fd168e6c3d834a1d7b79069feaf8d1994ba
raeesakhanam444 <raeesakhanam444@gmail.com>
2026-03-17 14:41:42 +0530

    made changes in pom.xml

--- seleniummaven/src/test/java/semaven/OrgJavaTest.java
+++ seleniummaven/src/test/java/semaven/OrgJavaTest.java
@@ -5,6 +5,17 @@ import org.testng.annotations.Test;
 public class OrgJavaTest {
 	@Test
 	public void createOrgTest() {
+		
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD); 
 		System.out.println("execute createOrgTest");
 	}
 	@Test
