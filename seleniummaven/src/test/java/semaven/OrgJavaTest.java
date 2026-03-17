c20c6f5433d8ed15f9126b8b41d87c4ac65cb3d2
raeesakhanam444 <raeesakhanam444@gmail.com>
2026-03-17 10:29:52 +0530

    1st push

---
+++ seleniummaven/src/test/java/semaven/OrgJavaTest.java
@@ -0,0 +1,15 @@
+package semaven;
+
+import org.testng.annotations.Test;
+
+public class OrgJavaTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrgTest");
+	}
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifycontactTest");
+	}
+
+}
